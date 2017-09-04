9f320b7909fcd2e91ce81f461640d96c34c3e4dd
eppineda <enrique.pineda@gmail.com>
2017-09-04 15:59:28 -0400

    clarify with type declarations

--- session/screen.ts
+++ session/screen.ts
@@ -10,14 +10,14 @@ export interface GameStatistics {
 
 // models a panel of information, a subdivided area of the screen
 export class Panel {
-    _screen
-    _title
-    _position
+    _screen:Screen
+    _title:string
+    _position:string
     _information:GameStatistics
-    _render
-    _shared
-    _minimized
-    constructor(screen, title, information) {
+    _render:Function
+    _shared:boolean
+    _minimized:boolean
+    constructor(screen:Screen, title:string, information:GameStatistics) {
         this._screen = screen
         this._title = title
         this._information = information
@@ -31,31 +31,31 @@ export class Panel {
     get position() { return this._position }
     set shared(flag) { this._shared = flag }
     get minimized() { return this._minimized }
-    minimize() {
-        this._minimized = true
-    }
+    minimize() { this._minimized = true } // todo: tell screen to rearrange
+    restore() { this._minimized = false } // todo: tell screen to rearrange
 } // Panel
 
 // the collection of all panels constitutes the entire "screen"
 
 export class Screen {
-    _panels
+    _screenOrganizer:ScreenOrganizer
+    _panels:Panel[]
     _layouts
-    constructor() {
+    constructor(sc:ScreenOrganizer) {
+        this._screenOrganizer = sc
         this._panels = []
         this._layouts = {}
     }
-    get layouts() { return Object.keys(this._layouts) }
+    get layouts():string[] { return Object.keys(this._layouts) }
     render(templateName) { this._layouts[templateName].render(this._panels) }
-    attach(panel, ScreenOrganizer) {
+    attach(panel:Panel) {
         this._panels.push(panel)
-        this._layouts =
-        ScreenOrganizer.options(this._panels.map(function(p) {
+        this._layouts = this._screenOrganizer.options(this._panels.map(function(p) {
             return !p.minimized
         }).length)
         return this._layouts
     }
-    minimize(panel) {
+    minimize(panel:Panel) {
         panel.minimize()
     }
 } // Screen
@@ -65,7 +65,11 @@ export class Screen {
  * subclass for each possible layout
  */
 export interface Layout {
-    _name:String
-    _template:String
-    render(panels:Object[]):void
+    _name:string
+    _template:string
+    render(panels:Panel[]):void
 } // Layout
+
+export interface ScreenOrganizer {
+    options(numPanels:number):{}
+}
