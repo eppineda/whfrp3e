5eec2139599e2b5bfa3f9ad7275e6fc56347beec
eppineda <enrique.pineda@gmail.com>
2017-09-04 01:00:21 -0400

    the idea is to allow rearrangement of all active panels on a grid layout

--- session/screen.ts
+++ session/screen.ts
@@ -7,7 +7,7 @@
 export class Information {
     render() {
     }
-}
+} // Information
 
 // models a panel of information, a subdivided area of the screen
 export class Panel {
@@ -17,6 +17,7 @@ export class Panel {
     _information
     _render
     _shared
+    _minimized
     constructor(screen, title, information) {
         this._screen = screen
         this._title = title
@@ -24,38 +25,54 @@ export class Panel {
         this._position = this._screen.attach(this)
         this._render = this._information.render
         this._shared = false // true to allow others to view
+        this._minimized = false // true when the info is not presented on-screen
     }
     get title() { return this._title }
     set title(t) { this._title = t }
     get position() { return this._position }
     set shared(flag) { this._shared = flag }
-}
+    get minimized() { return this._minimized }
+    minimize() {
+        this._minimized = true
+    }
+} // Panel
 
 // the collection of all panels constitutes the entire "screen"
 
-export default class Screen {
+export class Screen {
     _panels
+    _arrangements
     constructor() {
         this._panels = []
+        this._arrangements = {}
     }
-    render() {
-        for (let p of this._panels) {
-            p.render()
-        }
-    }
-    attach(panel) {
-        let reorganize = function() {
-/*
-todo - arrange the panels so that they're all readable and accessible
-    ...
-todo - arrange the panels so that they're all readable and accessible
-*/
-            for (let p of this._panels) {
-                p.render()
-            }
-        } // reorganize
-
+    get arrangements() { return Object.keys(this._arrangements) }
+    render(templateName) { this._arrangements[templateName].render(this._panels) }
+    attach(panel, ScreenOrganizer) {
         this._panels.push(panel)
-        reorganize()
+        this._arrangements =
+        ScreenOrganizer.options(this._panels.map(function(p) {
+            return !p.minimized
+        }).length)
+        return this.arrangements
     }
-}
+    minimize(panel) {
+        panel.minimize()
+    }
+} // Screen
+
+/**
+ * organizes a collection of visible panels into a grid layout
+ * subclass for each possible layout
+ */
+export class Arrangement {
+    _name
+    _template
+    constructor(name, template) {
+        this._name = name
+        this._template = template
+    }
+    get name() { return this._name }
+    render(panels) {
+    }
+} // Arrangement
